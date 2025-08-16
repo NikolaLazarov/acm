ae215dc332f90f398a38e7315741a21afaa76126
gourhwhaja <n.lazarovalex@gmail.com>
2025-08-16 15:24:19 +0300

    fix mobile close outside of picture

--- gallery.js
+++ gallery.js
@@ -2,8 +2,16 @@ import PhotoSwipeLightbox from './photoswipe-lightbox.esm.js';
 import PhotoSwipe from './photoswipe.esm.js';
 
 const lightbox = new PhotoSwipeLightbox({
-  gallery: '#main-gallery',
-  children: 'a',
-  pswpModule: PhotoSwipe
+    gallery: '#main-gallery',
+    children: 'a',
+    pswpModule: PhotoSwipe,
+    imageClickAction: (releasePoint, e) => { playpause() },
+    tapAction: (pt, evt) => {
+        if (evt.target.classList.contains('pswp__img')) {
+            if (isImage) playpause();
+        } else {
+            pswp.close();
+        }
+    }
 });
 lightbox.init();
